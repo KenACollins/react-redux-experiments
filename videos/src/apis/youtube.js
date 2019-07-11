4883eac7233b739afbd02891bb8c6428615b03e5
Ken A Collins <Ken@KenACollins.com>
2019-07-11 14:12:58 -0400

    Corrected vulnerability.

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-const KEY = 'AIzaSyCMo7gSnJPg_Tr5aF6T4306vKeNPYNNZAM';
+const key = require('./apiKeyDev');
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
-        key:KEY
+        key: key.youTubeAPIKey
     }
 });
